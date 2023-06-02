3f5eae7da75f2224c57d058f57f0813d1f5ed651
MehediHasan06 <mehedihasanmasud06@gmail.com>
2023-06-02 18:25:39 +0600

    testing husky

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request } from 'express';
 const app: Application = express();
 
-app.use(cors());
+    app.use(cors());
 
 //parser
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    app.use(express.urlencoded({ extended: true }));
 
 //Testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working Successfully');
+app.get('/', (req: Request, res: any) => {
+    res.send('Working Successfully');
 });
 
 export default app;
